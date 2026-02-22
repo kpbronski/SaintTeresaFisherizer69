7cd5904527cd8d807a89dc370c4c04dfa77b05da
Kevin Bronski <kpbronski@gmail.com>
2026-02-21 21:01:28 -0500

    Initial Commit

---
+++ SaintTeresaFisherizer69/SaintTeresaFisherizer69App.swift
@@ -0,0 +1,17 @@
+//
+//  SaintTeresaFisherizer69App.swift
+//  SaintTeresaFisherizer69
+//
+//  Created by Kevin Bronski on 2/21/26.
+//
+
+import SwiftUI
+
+@main
+struct SaintTeresaFisherizer69App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
